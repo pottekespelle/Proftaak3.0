57600367467fab4874d057437dd0f07bc6e2ddaa
[Niels] <[niellogics@gmail.com]>
2017-05-16 11:46:31 +0200

    stats index added

---
+++ stats/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Proftaak</title>
+	<link rel="stylesheet" type="text/css" href="http://meyerweb.com/eric/tools/css/reset/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script type="text/javascript" src="js/main.js"></script>
+</head>
+<body>
+<div>
+	<div class="header">
+        <a href="#"><img src="img/logo.png" class="logo"></a>
+        <a><img src="img/login.png" class="login"></a>
+    </div>
+
+</div>
+	<div class="footer">
+		<!-- left copyright right made by -->
+		<label class="copyright-footer">Copyright 2017 &copy; CSHUB</label>
+		<label class="madeby-footer">Made by: Niels v. Laarhoven &amp; Quinten Vis</label>
+	</div>
+</body>
+</html>
