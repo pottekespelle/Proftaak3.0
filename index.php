f1cc0704f4d90c14ced40ad8e8c0e10f77f8ef56
[Niels] <[niellogics@gmail.com]>
2017-05-16 11:09:38 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Proftaak</title>
+	<link rel="stylesheet" type="text/css" href="http://meyerweb.com/eric/tools/css/reset/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script type="text/javascript" src="js/main.js"></script>
+</head>
+<body>
+<div>
+	<div class="header">
+        <a><img src="img/logo.png" class="logo"></a>
+        <a><img src="img/login.png" class="login"></a>
+    </div>
+
+	<div class="wrapper">
+		<div class="form-input">
+			<label class="icon glyphicon glyphicon-pencil"></label>
+			<button id="convert" class="button glyphicon glyphicon-search"></button>
+			<input type="text" class="input" value="76561198344278706" />
+		</div>
+	</div>
+
+	
+</div>
+	<div class="footer">
+		<!-- left copyright right made by -->
+		<label class="copyright-footer">Copyright 2017 &copy; CSHUB</label>
+		<label class="madeby-footer">Made by Niels v. Laarhoven &amp; Quinten Vis</label>
+	</div>
+</body>
+</html>
